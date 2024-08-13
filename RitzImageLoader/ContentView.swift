b9b62863c41681d9e86cd23c0711e562c6497c57
MonicaKennedy92 <monicakennedyofficial@gmail.com>
2024-08-13 13:43:31 -0600

    Initial Commit

---
+++ RitzImageLoader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RitzImageLoader
+//
+//  Created by Monica on 2024-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RitzImageLoader/RitzImageLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RitzImageLoaderApp.swift
+//  RitzImageLoader
+//
+//  Created by Monica on 2024-08-13.
+//
+
+import SwiftUI
+
+@main
+struct RitzImageLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
